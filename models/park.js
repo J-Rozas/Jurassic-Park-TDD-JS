4e1774ead5fd384f9ee79f8d4647bd517a7d4aa3
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-05-04 07:25:08 +0100

    Create basic constructor for park

---
+++ models/park.js
@@ -0,0 +1,7 @@
+const Park = function (input_name, input_ticket_price, input_dinosaurs) {
+  this.name = input_name;
+  this.ticket_price = input_ticket_price;
+  this.dinosaurs = input_dinosaurs;
+}
+
+module.exports = Park;
