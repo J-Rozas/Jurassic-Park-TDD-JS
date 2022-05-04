4634903fad9e280fa608aa36e43ed07ba8d7a8a2
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-05-04 07:47:01 +0100

    Add function to retrieve most popular dino

--- models/park.js
+++ models/park.js
@@ -12,4 +12,18 @@ Park.prototype.removeDino = function (index) {
   this.dinosaurs.slice(index, 1);
 }
 
+Park.prototype.mostPopularDino = function () {
+  let mostPopular = null;
+  let baseVisitors = 0;
+
+  for (let dinosaur of this.dinosaurs) {
+    if (dinosaur.guestsAttractedPerDay > baseVisitors) {
+      mostPopular = dinosaur;
+      baseVisitors = dinosaur.guestsAttractedPerDay;
+    }
+  }
+
+  return mostPopular;
+}
+
 module.exports = Park;
