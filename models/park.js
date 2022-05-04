fc35c123fd94b0fb82d70813273bb48243a10590
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-05-04 07:29:27 +0100

    Modify styling (camel case)

--- models/park.js
+++ models/park.js
@@ -1,7 +1,7 @@
-const Park = function (input_name, input_ticket_price, input_dinosaurs) {
-  this.name = input_name;
-  this.ticket_price = input_ticket_price;
-  this.dinosaurs = input_dinosaurs;
+const Park = function (inputName, inputTicketPrice, inputDinosaurs) {
+  this.name = inputName;
+  this.ticketPrice = inputTicketPrice;
+  this.dinosaurs = inputDinosaurs;
 }
 
 module.exports = Park;
